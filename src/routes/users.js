77547ba860d3b935c2862a8f8529c49635256c80
JoestarXu <joestar.xu@grapecity.com>
2023-04-19 18:04:00 +0800

    first commit

---
+++ src/routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const { User } = require('../model/user');
+
+// 用户注册
+router.post('/register', async (req, res, next) => {
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+    res.send(user);
+});
+
+// 获取用户信息
+router.get('/info', async (req, res, next) => {
+    const user = await User.findOne({
+        _id: req.user_id
+    });
+    res.send({
+        code: 200,
+        data: user
+    });
+});
+
+// 获取用户列表
+router.get('/list', async (req, res, next) => {
+    const user = await User.find();
+    res.send({
+        code: 200,
+        msg: '获取成功',
+        data: user
+    });
+});
+
+module.exports = router;
